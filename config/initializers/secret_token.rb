bd4d7e47ad57cd5633361c87735b8acda98b17a1
Ruben Hernando <hernando.ruben@gmail.com>
2011-05-05 22:08:25 +0200

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forolabradores::Application.config.secret_token = '7c89b5046fa7417ffac5aad6f204ad7ccc7c4e711d120c99bcc700bf4cb39de63865a10256d3d918d0208a0f8d4731ba89fd86cd618565dac418982dea34ffc3'
